20ae97728c29d1f75523d1259f460c9063492909
Osama Aleem <aleemosama10@gmail.com>
2023-03-14 19:59:25 +0500

    api-restored-to-working-condition

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Web.Http;
 using System.Web.Http.Description;
 using mob_monitoring_api.Models;
@@ -16,99 +17,26 @@ namespace mob_monitoring_api.Controllers
     {
         private FYP_DBEntities db = new FYP_DBEntities();
 
-        // GET: api/Users
-<<<<<<< HEAD
         [HttpPost]
         public HttpResponseMessage Register(User user)
-=======
-        public IQueryable<User> GetUser()
->>>>>>> parent of a868ab5 (added-authentication-functionality)
         {
-            return db.User;
-        }
-
-<<<<<<< HEAD
-        [HttpPost]
-        public HttpResponseMessage Login(User user)
-=======
-        // GET: api/Users/5
-        [ResponseType(typeof(User))]
-        public IHttpActionResult GetUser(int id)
->>>>>>> parent of a868ab5 (added-authentication-functionality)
-        {
-            User user = db.User.Find(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(user);
-        }
-
-        // PUT: api/Users/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutUser(int id, User user)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != user.UserID)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(user).State = EntityState.Modified;
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/Users
-        [ResponseType(typeof(User))]
-        public IHttpActionResult PostUser(User user)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
             db.User.Add(user);
             db.SaveChanges();
-
-            return CreatedAtRoute("DefaultApi", new { id = user.UserID }, user);
+            return new HttpResponseMessage(HttpStatusCode.OK);
         }
-
-        // DELETE: api/Users/5
-        [ResponseType(typeof(User))]
-        public IHttpActionResult DeleteUser(int id)
+        [HttpPost]
+        public HttpResponseMessage Login(User user)
         {
-            User user = db.User.Find(id);
-            if (user == null)
+            String role = "Not Found";
+            var u = db.User.Where(x => x.Email == user.Email && x.Password == user.Password).Select(x => x.Role).FirstOrDefault();
+            if (u != null)
             {
-                return NotFound();
+                role = u.ToString();
             }
-
-            db.User.Remove(user);
-            db.SaveChanges();
-
-            return Ok(user);
+            string message = $"{{\"message\": \"{role}\"}}";
+            HttpResponseMessage res = new HttpResponseMessage(HttpStatusCode.OK);
+            res.Content = new StringContent(message, Encoding.UTF8, "application/json");
+            return res;
         }
 
         protected override void Dispose(bool disposing)
