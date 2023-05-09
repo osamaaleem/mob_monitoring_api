fec51941583a2e7c765117f3ef1e05a731eaa4bc
Osama Aleem <aleemosama10@gmail.com>
2023-05-09 14:34:35 +0500

    proxy-creation-false

--- Controllers/TimelineController.cs
+++ Controllers/TimelineController.cs
@@ -1,13 +1,18 @@
-﻿using System;
+﻿using mob_monitoring_api.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace mob_monitoring_api.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*", exposedHeaders: "X-My-Header")]
     public class TimelineController : ApiController
     {
+        private FYP_DBEntities db = new FYP_DBEntities();
+
     }
 }
