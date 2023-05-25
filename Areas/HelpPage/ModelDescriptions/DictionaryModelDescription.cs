41411e587106b6a7c80a6a2a5479acabc6a57f26
Osama Aleem <aleemosama10@gmail.com>
2023-05-25 12:04:05 +0500

    Revert "debloat-code-repository"
    This reverts commit c9c685e0f95f7dba064c9b71b360ad2af2320194.

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace mob_monitoring_api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mob_monitoring_api.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
