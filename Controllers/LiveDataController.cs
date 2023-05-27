078409f9339a2cc3805d1374ad7dc961619aba08
Osama Aleem <aleemosama10@gmail.com>
2023-05-27 18:34:27 +0500

    update-package-install-signal-ir

---
+++ Controllers/LiveDataController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace mob_monitoring_api.Controllers
+{
+    public class LiveDataController : ApiController
+    {
+
+    }
+}
