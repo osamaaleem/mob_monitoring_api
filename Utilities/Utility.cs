d272ca23ddab6e0d95751852701f50252e2f8887
Talha Bin Talib <66553865+Talha7852@users.noreply.github.com>
2023-06-14 13:18:23 +0500

    Revert "mehreen-problem"
    This reverts commit 1b278199fafb19cf2e377e2fdf759f35a356a221.

--- Utilities/Utility.cs
+++ Utilities/Utility.cs
@@ -1,96 +1,56 @@
-﻿using System;
+﻿using mob_monitoring_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
 
-namespace CalculateDistanceNamespace
+namespace mob_monitoring_api.Utilities
 {
-    public class LatLng
+    public class Utility
     {
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-    }
-
-    public class CalculateDistanceClass
-    {
-        private double DegreesToRadians(double degrees)
+        public bool CheckPointInPolygon(RedZoneCoordinates[] polygon, RedZoneCoordinates point)
         {
-            return degrees * Math.PI / 180;
-        }
+            PointF[] lpf = new PointF[100];
 
-        public double CalculateDistance(LatLng point1, LatLng point2)
-        {
-            const double earthRadius = 6371; // Radius of the Earth in kilometers
-
-            double lat1 = DegreesToRadians(point1.Latitude);
-            double lon1 = DegreesToRadians(point1.Longitude);
-            double lat2 = DegreesToRadians(point2.Latitude);
-            double lon2 = DegreesToRadians(point2.Longitude);
-
-            double dlon = lon2 - lon1;
-            double dlat = lat2 - lat1;
-
-            double a = Math.Pow(Math.Sin(dlat / 2), 2) +
-                Math.Cos(lat1) * Math.Cos(lat2) * Math.Pow(Math.Sin(dlon / 2), 2);
-            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
-
-            double distance = earthRadius * c; // Distance in kilometers
-            return distance;
-        }
-
-        public double CalculateDistanceToPolygon(LatLng point, LatLng[] polygonPoints)
-        {
-            double minDistance = double.PositiveInfinity;
-
-            for (int i = 0; i < polygonPoints.Length; i++)
+            for (int i = 0; i < polygon.Count(); i++)
             {
-                LatLng p1 = polygonPoints[i];
-                LatLng p2 = polygonPoints[(i + 1) % polygonPoints.Length];
 
-                double distance = CalculateDistanceToSegment(point, p1, p2);
-                minDistance = Math.Min(distance, minDistance);
+                lpf[i] = ConvertToPoint((double)polygon[i].RedZoneLat, (double)polygon[i].RedZoneLon);
             }
 
-            return minDistance;
+            PointF pf = ConvertToPoint((double)point.RedZoneLat, (double)point.RedZoneLon);
+
+
+            bool r = IsPointInPolygon(lpf, pf);
+            return r;
         }
 
-        public double CalculateDistanceToSegment(LatLng point, LatLng segmentStart, LatLng segmentEnd)
+        public PointF ConvertToPoint(double lat, double lon)
         {
-            double segmentLength = CalculateDistance(segmentStart, segmentEnd);
+            double R = 6371;
 
-            if (segmentLength == 0)
+            double x = R * Math.Cos(lat) * Math.Cos(lon);
+            double y = R * Math.Cos(lat) * Math.Sin(lon);
+
+            return new PointF(Convert.ToSingle(x), Convert.ToSingle(y));
+        }
+
+        public bool IsPointInPolygon(PointF[] polygon, PointF point)
+        {
+
+
+
+            bool isInside = false;
+            for (int i = 0, j = polygon.Length - 1; i < polygon.Length; j = i++)
             {
-                return CalculateDistance(point, segmentStart);
+                if (((polygon[i].Y > point.Y) != (polygon[j].Y > point.Y)) &&
+                (point.X < (polygon[j].X - polygon[i].X) * (point.Y - polygon[i].Y) / (polygon[j].Y - polygon[i].Y) + polygon[i].X))
+                {
+                    isInside = !isInside;
+                }
             }
-
-            double t = Math.Max(0, Math.Min(1, DotProduct(point, segmentStart, segmentEnd)));
-            LatLng projectedPoint = new LatLng
-            {
-                Latitude = segmentStart.Latitude + t * (segmentEnd.Latitude - segmentStart.Latitude),
-                Longitude = segmentStart.Longitude + t * (segmentEnd.Longitude - segmentStart.Longitude)
-            };
-
-            return CalculateDistance(point, projectedPoint);
-        }
-
-        public double DotProduct(LatLng point, LatLng segmentStart, LatLng segmentEnd)
-        {
-            double x1 = segmentStart.Latitude - point.Latitude;
-            double y1 = segmentStart.Longitude - point.Longitude;
-            double x2 = segmentEnd.Latitude - point.Latitude;
-            double y2 = segmentEnd.Longitude - point.Longitude;
-
-            return (x1 * x2 + y1 * y2) / CalculateDistance(segmentStart, segmentEnd);
+            return isInside;
         }
     }
-
- /*   public class Program
-    {
-        public static void Main(string[] args)
-        {
-            // Usage example:
-            CalculateDistanceClass calculator = new CalculateDistance();
-            LatLng point1 = new LatLng { Latitude = 40.7128, Longitude = -74.0060 };
-            LatLng point2 = new LatLng { Latitude = 34.0522, Longitude = -118.2437 };
-            double distance = calculator.CalculateDistance(point1, point2);
-            Console.WriteLine($"Distance: {distance} km");
-        }
-    }*/
 }
